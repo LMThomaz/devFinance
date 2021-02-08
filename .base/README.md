2a3b54eed742227f1fe537210414bf41c5383eec
Leonardo Thomaz <33641830+LMThomaz@users.noreply.github.com>
2021-02-08 11:33:25 -0300

    add folder .base to project

---
+++ .base/README.md
@@ -0,0 +1,106 @@
+<h4 align="center">
+    <img alt="" src=".github/logo.svg" />
+    <br/>
+    Gerencia suas finanças
+    <br>
+</h4>
+
+<p align="center">
+  <br>
+  <img alt="Language count" src="https://img.shields.io/github/repo-size/LMThomaz/devFinance"/>
+
+  <a href="https://rocketseat.com.br">
+    <img alt="Made by Rocketseat" src="https://img.shields.io/badge/made%20by-Rocketseat-%237519C1">
+  </a>
+
+  <a href="https://github.com/LMThomaz/devFinance/commits/master">
+    <img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/LMThomaz/devFinance">
+  </a>
+
+  <img alt="License" src="https://img.shields.io/github/license/LMThomaz/devFinance">
+</p>
+
+---
+
+## :mag: Sobre o projeto
+
+Feito de desenvolvedor para desenvolvedor, sistema para seu controle financeiro :computer::dollar:  
+Desenvolvido durante a Maratona Discover, oferecida pela [Rocketseat :rocket:][url-rocketseat]  
+A maratona detêm o conteúdo prático de todo os cursos oferecidos na plataforma de forma gratuíta, também oferecendo desáfios, insights, hacks e uma explicação concisa :coffee:
+
+---
+
+## :art: O protótipo?
+
+O layout da aplicação está disponível no [Figma][url-figma]
+![Capa devFinance](.github/capa.png)
+
+---
+
+## :hammer: Tecnologias?
+
+Utilizando a tríade base da web:
+
+- HTML
+- CSS
+- JavaScript
+
+---
+
+## :electric_plug: Como usar?
+
+É **necessário** que tenha instalado em sua máquina:
+
+- [Git][url-git]
+- Editor de texto (recomendação: [VSCode][url-vs])
+
+## :package: Como clonar a aplicação?
+
+```bash
+# Clonar o repositório
+$ git clone https://github.com/LMThomaz/devFinance
+```
+
+## :coffee: Como posso ajudar?
+
+1. Faça o Fork deste repositório
+2. Comece uma branch com sua feature
+
+```bash
+$ git checkout -b minha-feature
+```
+
+3. Confirme (Commit) seus feitos
+
+```bash
+$ git commit -m 'Minha nova feature'
+```
+
+4. Suba (push) sua branch
+
+```bash
+$ git push origin minha-feature
+```
+
+## :mortar_board: Quem ministrou ?
+
+As aulas foram ministradas pelo [Mayk Brito][mayk], durante as aulas da **Maratona Discover** :rocket:
+
+## :page_with_curl: Licença
+
+O projeto está utilizando a licença MIT. Confira [LICENSE][license] para mais detalhes.
+
+---
+
+<h4 align="center">
+Feito com 💜 por <a href="https://www.linkedin.com/in/leonardo-thomaz/" target="_blank">Leonardo Thomaz</a>
+</h4>
+
+[url-figma]: https://www.figma.com/file/vBNDmSI71wooNPWbL5uc6L/dev.finance-Maratona-Discover?node-id=0%3A1/
+[url-rocketseat]: https://rocketseat.com.br/
+[url-git]: https://git-scm.com/
+[url-vs]: https://code.visualstudio.com/
+[url-npm]: https://www.npmjs.com/
+[url-yarn]: https://yarnpkg.com/
+[mayk]: https://github.com/maykbrito
+[license]: https://github.com/LMThomaz/devFinance/blob/master/LICENSE.md
---
+++ .base/scripts.js
@@ -0,0 +1,218 @@
+const Modal = {
+  toggle() {
+    document.querySelector(".modal-overlay").classList.toggle("active");
+  },
+};
+
+const Storage = {
+  get() {
+    return JSON.parse(localStorage.getItem("dev.finances:transactions")) || [];
+  },
+
+  set(transactions) {
+    localStorage.setItem(
+      "dev.finances:transactions",
+      JSON.stringify(transactions)
+    );
+  },
+};
+
+const Transaction = {
+  all: Storage.get(),
+
+  add(transaction) {
+    Transaction.all.push(transaction);
+    App.reload();
+  },
+
+  remove(index) {
+    Transaction.all.splice(index, 1);
+    App.reload();
+  },
+
+  incomes() {
+    let income = 0;
+
+    Transaction.all
+      .filter(({ amount }) => amount > 0)
+      .forEach(({ amount }) => (income += amount));
+
+    return income;
+  },
+
+  expenses() {
+    let expense = 0;
+
+    Transaction.all
+      .filter(({ amount }) => amount < 0)
+      .forEach(({ amount }) => (expense += amount));
+
+    return expense;
+  },
+
+  total() {
+    return Transaction.incomes() + Transaction.expenses();
+  },
+};
+
+const DOM = {
+  transactionsContainer: document.querySelector("#dataTable tbody"),
+
+  addTransaction(transaction, index) {
+    const tr = document.createElement("tr");
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
+    tr.dataset.index = index;
+
+    DOM.transactionsContainer.appendChild(tr);
+  },
+
+  innerHTMLTransaction({ description, amount, date }, index) {
+    const classCSS = amount > 0 ? "income" : "expense";
+
+    const amountCurrency = Utils.formatCurrency(amount);
+
+    const html = `
+      <td class="description">${description}</td>
+      <td class=${classCSS}>${amountCurrency}</td>
+      <td class="date">${date}</td>
+      <td>
+        <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação" />
+      </td>
+    `;
+
+    return html;
+  },
+
+  updateBalance() {
+    document.getElementById("incomeDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.incomes()
+    );
+    document.getElementById("expenseDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.expenses()
+    );
+    document.getElementById("totalDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.total()
+    );
+  },
+
+  clearElement(element) {
+    element.innerHTML = "";
+  },
+
+  clearTransactions() {
+    DOM.clearElement(DOM.transactionsContainer);
+  },
+
+  renderTransactions(allTransactions) {
+    DOM.clearTransactions();
+    allTransactions.forEach(DOM.addTransaction);
+  },
+};
+
+const Utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? "-" : "";
+
+    value = String(value).replace(/\D/g, "");
+
+    value = Number(value) / 100;
+
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+
+    return signal + value;
+  },
+
+  formatAmount(value) {
+    value = Number(value) * 100;
+
+    return value;
+  },
+
+  formatDate(date) {
+    const splittedDate = date.split("-");
+    const [year, month, day] = splittedDate;
+
+    return `${day}/${month}/${year}`;
+  },
+
+  capitalizeFirstLetter(value) {
+    return `${value.charAt(0).toUpperCase()}${value.slice(1)}`;
+  },
+};
+
+const Form = {
+  description: document.querySelector("input#description"),
+  amount: document.querySelector("input#amount"),
+  date: document.querySelector("input#date"),
+
+  getValues() {
+    return {
+      description: Form.description.value,
+      amount: Form.amount.value,
+      date: Form.date.value,
+    };
+  },
+
+  validateFields() {
+    const { description, amount, date } = Form.getValues();
+
+    if (description.trim() === "" || amount.trim() === "" || date === "")
+      throw new Error("Por favor, preencha todos os campos");
+  },
+
+  formatValues() {
+    let { description, amount, date } = Form.getValues();
+
+    description = Utils.capitalizeFirstLetter(description).trim();
+
+    amount = Utils.formatAmount(amount);
+
+    date = Utils.formatDate(date);
+
+    return {
+      description,
+      amount,
+      date,
+    };
+  },
+
+  saveTransaction(transaction) {
+    Transaction.add(transaction);
+  },
+
+  clear() {
+    Form.description.value = "";
+    Form.amount.value = "";
+    Form.date.value = "";
+  },
+
+  submit(event) {
+    event.preventDefault();
+
+    try {
+      Form.validateFields();
+      const transaction = Form.formatValues();
+      Form.saveTransaction(transaction);
+      Form.clear();
+      Modal.toggle();
+    } catch (err) {
+      alert(err.message);
+    }
+  },
+};
+
+const App = {
+  init() {
+    DOM.renderTransactions(Transaction.all);
+    Storage.set(Transaction.all);
+    DOM.updateBalance();
+  },
+  reload() {
+    App.init();
+  },
+};
+
+App.init();
